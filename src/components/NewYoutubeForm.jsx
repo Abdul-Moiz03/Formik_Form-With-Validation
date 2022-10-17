78f7ad0c7e0d9f6983e334e4cc82dbcd939429b2
Abdul Moiz <moizrauf03@gmail.com>
2022-10-18 01:05:53 +0500

    text field,render props

--- src/components/NewYoutubeForm.jsx
+++ src/components/NewYoutubeForm.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
 import * as Yup from "yup";
 const initialValues = {
   name: "moiz",
   email: "",
   channel: "",
+  comments: "",
+  adress: {
+    city: "",
+    country: "",
+  },
+  Phone: ["", ""],
+  phNumbers: [""],
 };
 //this automaticcly takes values
 // const onSubmit = (values) => {
@@ -35,6 +42,11 @@ const validationSchema = Yup.object({
   name: Yup.string().required("Requried!"),
   email: Yup.string().email("Invalid Email Formate").required("Requried!"),
   channel: Yup.string().required("Requried!"),
+  comments: Yup.string().required("Requried!"),
+  adress: Yup.object().shape({
+    city: Yup.string().required("Requried!"),
+    country: Yup.string().required("Requried!"),
+  }),
 });
 const NewYoutubeForm = () => {
   //   const formik = useFormik({
@@ -64,7 +76,12 @@ const NewYoutubeForm = () => {
             // onChange={formik.handleChange}
             // value={formik.values.name}
           ></Field>
-          <ErrorMessage name="name" />
+
+          <ErrorMessage name="name">
+            {/* to apply class and wrap the msg in div */}
+            {/* we can also make a different componet then pass in ErrorMessage as prop */}
+            {(error) => <div className="error">{error}</div>}
+          </ErrorMessage>
           {/* {formik.errors.name && formik.touched.name ? (
             <div className="error">{formik.errors.name}</div>
           ) : null} */}
@@ -80,7 +97,9 @@ const NewYoutubeForm = () => {
             // onChange={formik.handleChange}
             // value={formik.values.email}
           ></Field>
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email">
+            {(error) => <div className="error">{error}</div>}
+          </ErrorMessage>
           {/* {formik.errors.name && formik.touched.email ? (
             <div className="error">{formik.errors.email}</div>
           ) : null} */}
@@ -96,10 +115,109 @@ const NewYoutubeForm = () => {
             // onChange={formik.handleChange}
             // value={formik.values.channel}
           ></Field>
-          <ErrorMessage name="channel" />
+          <ErrorMessage name="channel">
+            {(error) => <div className="error">{error}</div>}
+          </ErrorMessage>
           {/* {formik.errors.name && formik.touched.channel ? (
             <div className="error">{formik.errors.channel}</div>
           ) : null} */}
+        </div>
+        {/* <div className="form-control">
+          <label htmlFor="adress"> Adress</label>
+          <Field type="text" name="adress" id="adress"></Field>
+          <ErrorMessage name="adress">
+            {(error) => <div className="error">{error}</div>}
+          </ErrorMessage>
+        </div> */}
+        <div className="form-control">
+          <label htmlFor="city"> City</label>
+          <Field type="text" name="adress.city" id="city"></Field>
+          <ErrorMessage name="adress.city">
+            {(error) => <div className="error">{error}</div>}
+          </ErrorMessage>
+        </div>
+        <div className="form-control">
+          <label htmlFor="country"> Country</label>
+          <Field type="text" name="adress.country" id="country"></Field>
+          <ErrorMessage name="adress.country">
+            {(error) => <div className="error">{error}</div>}
+          </ErrorMessage>
+        </div>
+        <div className="form-control">
+          <label htmlFor="phone">Phone No</label>
+          <Field // field component takes as prop to decide what elemt to render
+            type="text"
+            name="Phone[0]"
+            id="phone"
+          ></Field>
+
+          {/* <ErrorMessage name="Phone[0]">
+            {(error) => <div className="error">{error}</div>}
+          </ErrorMessage> */}
+        </div>
+        <div className="form-control">
+          <label htmlFor="Phone2">Phone No2</label>
+          <Field // field component takes as prop to decide what elemt to render
+            type="text"
+            name="Phone[1]"
+            id="phone2"
+          ></Field>
+
+          {/* <ErrorMessage name="phone2">
+            {(error) => <div className="error">{error}</div>}
+          </ErrorMessage> */}
+        </div>
+        {/* Dynamically Adding phone Numbers */}
+        <div className="form-control">
+          <label htmlFor="phNumbers">Phone Noss</label>
+          <FieldArray name="phNumbers">
+            {(fieldprop) => {
+              const { push, remove, form } = fieldprop;
+              const { values } = form;
+              const { phNumbers } = values;
+
+              return (
+                <div>
+                  {phNumbers.map((phnumber, index) => (
+                    <div key={index}>
+                      <Field name={`phNumbers[${index}]`} />;
+                      <button
+                        type="button"
+                        onClick={() => {
+                          push("");
+                        }}
+                      >
+                        +
+                      </button>
+                      {index > 0 && (
+                        <button
+                          type="button"
+                          onClick={() => {
+                            remove(index);
+                          }}
+                        >
+                          -
+                        </button>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              );
+            }}
+          </FieldArray>
+        </div>
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field // field component takes as prop to decide what elemt to render
+            as="Textarea"
+            type="text"
+            name="comments"
+            id="comments"
+          ></Field>
+
+          <ErrorMessage name="comments">
+            {(error) => <div className="error">{error}</div>}
+          </ErrorMessage>
         </div>
         <button type="submit">Submit</button>{" "}
       </Form>
